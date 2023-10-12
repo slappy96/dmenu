562fe8dc1b47586effd34665812ffba8ee265b60
slappy <jqmunos@gmail.com>
2023-10-12 13:52:58 -0500

    changed color to match red/black

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#88c0d0", "#282c34" },
-	[SchemeSel] = { "#000000", "#90b1b1" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#890D03" },
+	[SchemeOut] = { "#890D03", "#444444" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
